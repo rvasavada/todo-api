1911b4368b4fc383414fc7a11b823ecce20f17e3
Rhut Vasavada <rhutvasavada@gmail.com>
2017-07-31 21:04:35 -0500

    added delete

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
+var _ = require('underscore');
 
 var bodyParser = require('body-parser');
 
@@ -20,16 +21,10 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var toDoId = parseInt(req.params.id, 10);
-	var matchedTodos;
+	var matchedTodo = _.findWhere(todos, {id: toDoId});
 
-	for(var i = 0; i < todos.length; i++) {
-		if (todos[i].id === toDoId) {
-			matchedTodos = todos[i];
-		}	
-	}
-
-	if (matchedTodos) {
-		res.json(matchedTodos);
+	if (matchedTodo) {
+		res.json(matchedTodo);
 	} else {
 		res.status(404).send();	
 	}
@@ -37,7 +32,13 @@ app.get('/todos/:id', function(req, res) {
 
 //POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'completed', 'description');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	console.log('description: ' + body.description);
 
@@ -45,6 +46,19 @@ app.post('/todos', function(req, res) {
 	todos.push(body);
 
 	res.json(body);
+});
+
+//DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+	var toDoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: toDoId});
+
+	if (matchedTodo) {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	} else {
+		res.status(404).json({"error": 'couldn\'t find id'});	
+	}
 });
 
 app.listen(PORT, function() {
