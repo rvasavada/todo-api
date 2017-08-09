0c129c0235ed731c583bffaef75ed6ee909a89f8
Rhut Vasavada <rhutvasavada@gmail.com>
2017-08-08 21:36:21 -0400

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -230,6 +230,17 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function (user) {
+		res.status(200).json(user.toJSON());
+	}).catch(function (error) {
+		res.status(400).json(error);
+	});
+
+});
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('express listening on port ' + PORT + '!');
