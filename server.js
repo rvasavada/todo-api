4bf6d5b72bc0b553955dcb16b6f77f9f7e4c976a
Rhut Vasavada <rhutvasavada@gmail.com>
2017-08-09 19:19:34 -0400

    added DELETE /users/login

--- server.js
+++ server.js
@@ -257,31 +257,41 @@ app.put('/todos/:id',  middleware.requireAuthentication, function(req, res) {
 
 });
 
-app.post('/users', function(req, res) {
+app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function (user) {
-		res.status(200).json(user.toPublicJSON());
-	}).catch(function (error) {
-		res.status(400).json(error);
+		res.json(user.toPublicJSON());
+	}, function (e) {
+		res.status(400).json(e);
 	});
-
 });
 
-//POST /users/login
-app.post('/users/login', function(req, res) {
+// POST /users/login
+app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
-	}, function () {
+		return db.token.create({
+			token: token
+		});
+	}).then(function (tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function () {
 		res.status(401).send();
+	});
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function (req, res) {
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function () {
+		res.status(500).send();
 	});
 });
 
